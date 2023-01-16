10fb25ea0c4589e2a0fca9d6a4fde8b9c976e62a
ANDRII SYNII <79702542+Angri154@users.noreply.github.com>
2023-01-16 18:14:15 +0200

    dfv

--- index.53a43afc.js
+++
@@ -1,2 +0,0 @@
-window.addEventListener("scroll",(e=>{document.documentElement.style.setProperty("--scrollTop",`${this.scrollY}px`)})),gsap.registerPlugin(ScrollTrigger,ScrollSmoother),ScrollSmoother.create({wrapper:".wrapper",content:".content"});
-//# sourceMappingURL=index.53a43afc.js.map
